131f0c9ca26e543132eb0af0c063a288c19427dc
Ruben Moor <ruben.moor@gmail.com>
2016-04-20 13:46:36 +0200

    moved to logger settings

--- src/Diener/Logger.hs
+++ src/Diener/Logger.hs
@@ -3,12 +3,13 @@
 module Diener.Logger
   ( withLogger
   , LogFunction
+  , Settings (..)
   ) where
 
 import           Control.Exception.Base (bracket)
 import           Control.Monad.Logger
 import           Control.Monad.Reader
-
+import Data.Default (Default, def)
 import qualified Data.ByteString.Char8  as Char8 (putStrLn)
 import           Data.Monoid            ((<>))
 import           Data.Time
@@ -22,21 +23,30 @@ import           System.Log.FastLogger  (LoggerSet, defaultBufSize, fromLogStr,
 
 type LogFunction = Loc -> LogSource -> LogLevel -> LogStr -> IO ()
 
-withLogger :: FilePath -> LogLevel -> (LogFunction -> LoggingT IO a) -> IO a
-withLogger logFile logLevel action =
+data Settings = Settings
+    { filePath :: FilePath
+    , logLevel :: LogLevel
+    , noConsoleLogging :: Bool
+    }
+
+instance Default Settings where
+    def = Settings "logfile.log" LevelDebug False
+
+withLogger :: Settings -> (LogFunction -> LoggingT IO a) -> IO a
+withLogger (Settings logFile level noConsLogging) action =
   bracket (newFileLoggerSet defaultBufSize logFile) rmLoggerSet $ \loggerSet -> do
-    let logFn = logMsg loggerSet logLevel
+    let logFn = logMsg loggerSet level noConsLogging
     runLoggingT (action logFn) logFn
 
-logMsg :: LoggerSet -> LogLevel -> LogFunction
-logMsg loggerSet maxLogLevel loc _ level msg =
-  when (level >= maxLogLevel) $ do
-    out <- getOutput
-    pushLogStr loggerSet (out <> "\n")
-    when (level >= LevelWarn) $ do
-      when (level == LevelError) $ setSGR [SetColor Foreground Vivid Red]
-      Char8.putStrLn $ fromLogStr out
-      when (level == LevelError) $ setSGR [Reset]
+logMsg :: LoggerSet -> LogLevel -> Bool -> LogFunction
+logMsg loggerSet maxLogLevel noConsLogging loc _ level msg =
+    when (level >= maxLogLevel) $ do
+      out <- getOutput
+      pushLogStr loggerSet (out <> "\n")
+      unless noConsLogging $ do
+          when (level == LevelError) $ setSGR [SetColor Foreground Vivid Red]
+          Char8.putStrLn $ fromLogStr out
+          when (level == LevelError) $ setSGR [Reset]
   where
     getOutput = do
       date <- getDate
